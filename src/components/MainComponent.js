70ba768f2ae3a5a27342ce8b58db8cddbf560cb2
Isha <isha.saxena@mountblue.io>
2018-11-11 00:01:26 +0530

    added featured dish,promotion and leader

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,7 +2,10 @@ import React, { Component } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Menu from './MenuComponent';
 import DishDetail from './DishDetailComponent';
-import DISHES from '../shared/dishes';
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
@@ -13,21 +16,20 @@ class Main extends Component {
     super(props);
     this.state = {
       dishes: DISHES,
-      selectedDish: null
+      comments: COMMENTS,
+      promotions: PROMOTIONS,
+      leaders: LEADERS,
     };
-  }
-
-  onDishSelect(dishId) {
-    this.setState({
-      selectedDish: dishId
-    })
   }
 
   render() {
     const HomePage = () => {
-      return (
-        <Home
-        />
+      return(
+          <Home 
+              dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+              promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+              leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+          />
       );
     }
 
