0d0eacd34fc5842ea49598625acd1935b00d00a5
Isha <isha.saxena@mountblue.io>
2018-11-11 21:32:03 +0530

    Added breadcrumbs

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
+import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import { ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 function RenderDish({ dish }) {
   if (dish != null)
@@ -49,11 +50,26 @@ const DishDetail = (props) => {
     return (
       <div className='container'>
         <div className='row'>
-          <RenderDish dish={props.dish} />
-          <RenderComments comments={props.dish ? props.dish.comments : null} />
+          <Breadcrumb>
+            <BreadcrumbItem><Link to='/menu'>Menu</Link>
+            </BreadcrumbItem>
+            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+          </Breadcrumb>
+          <div className='col-12'>
+            <h3>{props.dish.name}</h3>
+            <hr />
+          </div>
         </div>
-      </div>
-    )
+        <div className='row'>
+          <div className='col-12 col-md-5 m-1'>
+            <RenderDish dish={props.dish} />
+          </div>
+          <div className='col-12 col-md-5 m-1'>
+            <RenderComments comments={props.dish ? props.dish.comments : null} />
+          </div>
+        </div>
+      </div >
+    );
   else
     return (<div></div>)
 }
