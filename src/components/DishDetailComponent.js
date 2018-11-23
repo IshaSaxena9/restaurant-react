e6a77f99f71ed73098acfc41834801ef7d3b6c2e
Isha <isha.saxena@mountblue.io>
2018-11-23 12:43:18 +0530

    Fetch Post comment

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -43,7 +43,7 @@ function RenderDish(props) {
     );
 }
 
-function RenderComments({ comments, addComment, dishId }) {
+function RenderComments({ comments, postComment, dishId }) {
   if (comments != null)
     return (
       <div className="col-12 col-md-5 m-1">
@@ -59,7 +59,7 @@ function RenderComments({ comments, addComment, dishId }) {
           )}
         </ListGroup>
         <br></br>
-        <CommentForm dishId={dishId} addComment={addComment} />
+        <CommentForm dishId={dishId} postComment={postComment} />
       </div>
     );
   else
@@ -89,7 +89,7 @@ class CommentForm extends React.Component {
 
   handleSubmit(values) {
     this.toggleModal();
-    this.props.addComment(this.props.dishId, values.rating, values.name, values.comment);
+    this.props.postComment(this.props.dishId, values.rating, values.name, values.comment);
   }
 
   render() {
@@ -178,7 +178,7 @@ const DishDetail = (props) => {
         <div className='row'>
           <RenderDish {...props} dish={props.dish} />
           <RenderComments comments={props.comments}
-           addComment={props.addComment}
+           postComment={props.postComment}
            dishId={props.dish.id}
           />
         </div>
--- src/redux/ActionCreators.js
+++ src/redux/ActionCreators.js
@@ -1,16 +1,5 @@
 import * as ActionTypes from './ActionTypes';
-import { DISHES } from '../shared/dishes';
 import { baseUrl } from '../shared/baseUrl';
-
-export const addComment = (dishId, rating, author, comment) => ({
-    type: ActionTypes.ADD_COMMENT,
-    payload: {
-        dishId: dishId,
-        rating: rating,
-        author: author,
-        comment: comment
-    }
-});
 
 export const fetchDishes = () => (dispatch) => {
     dispatch(dishesLoading(true));
@@ -64,7 +53,7 @@ export const fetchComments = () => (dispatch) => {
                 throw errmes;
             })
         .then(response => response.json())
-        .then(comments => dispatch(addComments(comments)))
+        .then(comments => dispatch(addComment(comments)))
         .catch(error => dispatch(commentsFailed(error.message)));
 };
 
@@ -73,9 +62,9 @@ export const commentsFailed = (errmess) => ({
     payload: errmess
 });
 
-export const addComments = (comments) => ({
-    type: ActionTypes.ADD_COMMENTS,
-    payload: comments
+export const addComment = (comment) => ({
+    type: ActionTypes.ADD_COMMENT,
+    payload: comment
 });
 
 export const fetchPromos = () => (dispatch) => {
@@ -114,3 +103,38 @@ export const addPromos = (promos) => ({
     type: ActionTypes.ADD_PROMOS,
     payload: promos
 });
+
+export const postComment = (dishId, rating, author, comment) => (dispatch) => {
+
+    const newComment = {
+        dishId: dishId,
+        rating: rating,
+        author: author,
+        comment: comment
+    };
+    newComment.date = new Date().toISOString();
+    
+    return fetch(baseUrl + 'comments', {
+        method: "POST",
+        body: JSON.stringify(newComment),
+        headers: {
+          "Content-Type": "application/json"
+        },
+        credentials: "same-origin"
+    })
+    .then(response => {
+        if (response.ok) {
+          return response;
+        } else {
+          var error = new Error('Error ' + response.status + ': ' + response.statusText);
+          error.response = response;
+          throw error;
+        }
+      },
+      error => {
+            throw error;
+      })
+    .then(response => response.json())
+    .then(response => dispatch(addComment(response)))
+    .catch(error =>  { console.log('post comments', error.message); alert('Your comment could not be posted\nError: '+error.message); });
+};
